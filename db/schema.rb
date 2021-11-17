0989b077006a05e2a0bb9ea44c2a6605f1178978
saldinbeganovic <saldin393@gmail.com>
2021-11-17 17:39:27 +0100

    flatpickr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_120933) do
+ActiveRecord::Schema.define(version: 2021_11_16_123019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_120933) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.datetime "scheduled_for"
   end
 
   create_table "users", force: :cascade do |t|
